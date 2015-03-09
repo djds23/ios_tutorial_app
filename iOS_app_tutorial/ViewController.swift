f9d19c1bdef4e8e4f2a1aad3bec7c49db21c2645
Dean Silfen <dean.silfen@gmail.com>
2015-03-09 19:56:34 -0400

    Initial Commit

---
+++ iOS_app_tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS_app_tutorial
+//
+//  Created by Dean Silfen on 3/9/15.
+//  Copyright (c) 2015 LazyBits LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
